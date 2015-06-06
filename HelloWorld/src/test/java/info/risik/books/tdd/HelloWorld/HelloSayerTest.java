17016c6f4bceee4911623af2d608f07ca750ee11
Sergei Borisov <sergeybor@gmail.com>
2015-06-06 21:08:04 +0300

    Implement greeting test using QuickCheck framework

--- HelloWorld/src/test/java/info/risik/books/tdd/HelloWorld/HelloSayerTest.java
+++ HelloWorld/src/test/java/info/risik/books/tdd/HelloWorld/HelloSayerTest.java
@@ -1,13 +1,19 @@
 package info.risik.books.tdd.HelloWorld;
 
+import com.pholser.junit.quickcheck.ForAll;
 import org.junit.Test;
+import org.junit.contrib.theories.Theories;
+import org.junit.contrib.theories.Theory;
+import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * tdd-book-java
  * Created by Sergei Borisov on 06/06/15.
  */
+
+@RunWith(Theories.class)
 public class HelloSayerTest {
 
     @Test
@@ -25,5 +31,13 @@ public class HelloSayerTest {
     public void testGreetingString() throws Exception {
         HelloSayer sayer = new HelloSayer("World");
         assertEquals("Hello \"World\"", sayer.getGreetingString());
+    }
+
+    @Theory
+    public void greetingString(
+            @ForAll String whom
+    ) {
+        HelloSayer sayer = new HelloSayer(whom);
+        assertEquals(String.format("Hello \"%s\"", whom), sayer.getGreetingString());
     }
 }
